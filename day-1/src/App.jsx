4c1be0007fae1108b6e25910a478e41e91ba062a
vatsal panchal <panchalvatsal2025@gmail.com>
2026-01-08 23:42:57 +0530

    booom

--- day-1/src/App.jsx
+++ day-1/src/App.jsx
@@ -1,6 +1,15 @@
 import React from "react";
+
+function User() {
+  return <h1>hello vatsal</h1>;
+}
+
 const App = () => {
-  return <div>{1 + 1}</div>;
+  return (
+    <div>
+      <User />
+    </div>
+  );
 };
 
 export default App;
