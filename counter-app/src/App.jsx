770be00d0e8e4d08c0536d3c40cdaae9ef1556b2
vatsal panchal <panchalvatsal2025@gmail.com>
2026-01-09 19:13:24 +0530

    CODE

--- counter-app/src/App.jsx
+++ counter-app/src/App.jsx
@@ -1,17 +1,16 @@
 import React, { useState } from "react";
 const App = () => {
+  const [count, setcount] = useState(0);
 
-
-  const [count, setcount] = useState(0)
   return (
     <div>
       <h1>counter App</h1>
       <p>count : {count}</p>
 
       <div className="btn">
-        <button onClick={()=>setcount(count+1)}>increment</button>
-        <button onClick={()=>setcount(count-1)}>decrement</button>
-        <button onClick={()=>setcount(0)}>reset</button>
+        <button onClick={() => setcount(count + 1)}>increment</button>
+        <button onClick={() => setcount(count - 1)}>decrement</button>
+        <button onClick={() => setcount(0)}>reset</button>
       </div>
     </div>
   );
